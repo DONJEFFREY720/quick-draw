68b94145f38b26451c24f7d05eb1f0371c8d3ee7
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-05-31 19:04:13 +0530

    new code

--- main.js
+++ main.js
@@ -50,4 +50,38 @@ function setup(){
      canvas = createCanvas(300,300)
      canvas.center()
      background("white")
+     canvas.mouseReleased(classifyCanvas)
+     synth = window.speechSynthesis
+}
+
+function preload(){
+    classifier = ml5.imageClassifier("DoodleNet")
+}
+
+function draw(){
+     strokeWeight(10)
+     stroke(0)
+
+     if(mouseIsPressed) {
+          line(pmouseX,pmouseY,mouseX,mouseY)
+     }
+}
+
+function classifyCanvas(){
+     classifier.classify(canvas,gotResults)
+}
+
+function gotResults(error,results){
+     if (error) {
+          console.error(error)
+     }
+
+     else{
+          console.log(results)
+          document.getElementById("my_sketch").innerHTML = "LABEL : "+results[0].label
+          document.getElementById("confidence").innerHTML = "CONFIDENCE : "+Math.round(results[0].confidence*100) + '%'
+
+          utterThis = new SpeechSynthesisUtterance(results[0].label) 
+          synth.speak(utterThis)
+     }
 }
